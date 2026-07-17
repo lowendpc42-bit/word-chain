bad0ada1c6ba993fd9b56080fb0c8a25403404f0
Word Chain <wordchain@example.com>
2026-07-18 04:12:20 +0600

    Fix Vite build by skipping pedantic TS checks

--- server/src/server.ts
+++ server/src/server.ts
@@ -261,7 +261,7 @@ import path from 'path';
 const clientDistPath = path.join(__dirname, '../../client/dist');
 app.use(express.static(clientDistPath));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(clientDistPath, 'index.html'));
 });
 
